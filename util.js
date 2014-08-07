c10b0828d0f3c250136cef30aef8e6beb408be31
rpdg <rpdg@sina.cn>
2014-08-07 12:24:53 +0800

    fix

--- util.js
+++ util.js
@@ -297,10 +297,8 @@ document.onkeydown = function(evt){
 
 	//主页
 	if(evt.keyCode == 72 && x$.on.homepage){
-		if(x$.on.homepage){
-			location.href = x$.on.homepage ;
-			return false;
-		}
+		location.href = x$.on.homepage ;
+		return false;
 	}
 	//后退
 	if(evt.keyCode == 8){
